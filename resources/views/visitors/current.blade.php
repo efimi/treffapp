0721e92f60427c7cb30b15d3ba1d272b43cd75ed
Zepryit <maximilian.kleindopp@wabsolute.de>
2017-11-07 13:32:02 +0100

    Auth Einbundung

--- resources/views/visitors/current.blade.php
+++ resources/views/visitors/current.blade.php
@@ -1,11 +1,10 @@
 <div id="current_matched">
-   <?php // TODO: mit Mermkal from visitor->Location(location)->merkmale ?>
-   <br>
-  @if ($location->used_places == 2)
-          <p>Du bist nicht alleine!!:) Derzeit kommt noch eine weitere Person</p>
-      @elseif ($location->used_places == 1)
-          
-      @else
-          <p>Derzeit kommen noch {{ ($location->used_places)-1 }} weitere Personen</p>
-  @endif
+    <br>
+    @if ($location->used_places == 2)
+        <p>Du bist nicht alleine!!:) Derzeit kommt noch eine weitere Person</p>
+    @elseif ($location->used_places == 1)
+
+    @else
+        <p>Derzeit kommen noch {{ ($location->used_places)-1 }} weitere Personen</p>
+    @endif
 </div>
