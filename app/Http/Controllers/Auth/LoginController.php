862ba20011a57a53e1503a50a385854892640c05
Zepryit <maximilian.kleindopp@wabsolute.de>
2017-11-07 11:43:38 +0100

    Auth Einbundung

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 use Socialite;
 use App\User;
 
