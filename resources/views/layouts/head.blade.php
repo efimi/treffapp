0b3baaa0608bc34bdd6d9c935226f1d861e7d702
Reinhold Lehnhardt <reinhold.lehnhardt@wabsolute.de>
2017-12-01 10:58:19 +0100

    $@

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,12 +8,11 @@
 <title>PaderMeet - Deine Treffapp für Paderborn</title>
 <!-- Bootstrap core CSS -->
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}" ></script>
+<script src="{{ asset('js/tether.min.js') }}" ></script>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" ></script>
 <!-- Custom styles for this template -->
 <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/narrow-jumbotron.css" >
